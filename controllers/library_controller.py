e9d4722392e5a98804906e38cbaa3081ae1e471b
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2022-11-09 17:50:22 +0000

    update controller

--- controllers/library_controller.py
+++ controllers/library_controller.py
@@ -18,17 +18,23 @@ def books():
 @books_blueprint.route("/books/new")
 def new_book():
     authors = author_repository.select_all()
-    return render_template("books/new.html", all_authors=authors)
+    return render_template("books/new.html", all_authors=authors, error=False)
 
 
 @books_blueprint.route("/books", methods=["POST"])
 def create_book():
-    title = request.form["description"]
-    genre = request.form["genre"]
-    author_id = request.form["author_id"]
+    authors = author_repository.select_all()
 
-    author = author_repository.select(author_id)
-    book = Book(title, genre, author)
+    book_title = request.form["title"]
+    book_author = request.form["author"]
+    book_genre = request.form["genre"]
+
+    # Show error message if a form field is missing
+    if book_title == "" or book_author == "" or book_genre == "":
+        return render_template("books/new.html", all_authors=authors, error=True) 
+
+    author = author_repository.select(book_author)
+    book = Book(book_title, book_genre, author)
 
     book_repository.save(book)
     return redirect("/books")
@@ -63,3 +69,49 @@ def update_book(id):
 def delete_book(id):
     book_repository.delete(id)
     return redirect("/books")
+
+
+
+
+
+
+@app.route("/books/search", methods=["GET"])
+def search_book():
+    title = request.args.get("title")
+    author = request.args.get("author")
+    genre = request.args.get("genre")
+
+    # Return a list of books that matches the search
+    updated_books_list = library.book_list.copy()
+    for book in library.book_list:
+        if title and title not in book.title:
+            updated_books_list.remove(book)
+            continue
+        if author and author not in book.author:
+            updated_books_list.remove(book)
+            continue
+        if genre and genre not in book.genre:
+            updated_books_list.remove(book)
+            continue
+
+    return render_template("books.html", books=updated_books_list, error=False)
+
+@app.route('/books/<hyphenated_title>/delete', methods=['POST'])
+def delete_book(hyphenated_title):
+    book = library.get_book(hyphenated_title)
+    library.remove_book(book)
+    return redirect("/books")
+
+@app.route('/books/<hyphenated_title>/update', methods=['POST'])
+def update_book_check_out_status(hyphenated_title):
+    # Update the check-out status based on user selection
+    book = library.get_book(hyphenated_title)
+    status = request.form.getlist("check-out-status")
+    library.update_check_out_status(book, status)
+
+    # Load page where the form submission occurred
+    location = request.args.get("loc")
+    if location == "book":
+        return render_template("book.html", book=book)
+    else:
+        return redirect("/books")
